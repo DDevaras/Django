f706a6561e733e336ddc1c94a1b1eb2d5842eb4d
DDevaras <59032984+DDevaras@users.noreply.github.com>
2023-01-14 19:57:07 -0800

    Update settings.py

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'Heat%65phz83&k447a773s2^z(f4uhq%x-#yq-0$lxffWOM'
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
